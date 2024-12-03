2060b0b74cbe146195975bac15ffbf411f1dada6
Dominik Sumer <domi.sumer@gmail.com>
2024-12-03 18:23:21 +0100

    added peer makers replies algos

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -4,11 +4,13 @@ import {
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import * as peerMakersPosts from './peer-makers-posts'
+import * as peerMakersReplies from './peer-makers-replies'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
   [peerMakersPosts.shortname]: peerMakersPosts.handler,
+  [peerMakersReplies.shortname]: peerMakersReplies.handler,
 }
 
 export default algos
